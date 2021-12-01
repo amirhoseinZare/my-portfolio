69f5dcb10f25d0be245008d46529a25567dc97e7
amir <zareamirhussein@gmail.com>
2021-12-02 02:22:22 +0330

    finish all style and script bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
-import '../styles/bootstrap.css'
-import '../styles/slicker.css'
-import '../styles/animate.css'
+// import '../styles/globals.css'
+// import '../styles/bootstrap.css'
+// import '../styles/slicker.css'
+// import '../styles/animate.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
