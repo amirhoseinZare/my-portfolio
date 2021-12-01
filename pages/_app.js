55114269d3b8fb1044c89697d818df5d51c75d9f
amir <zareamirhussein@gmail.com>
2021-12-01 23:12:46 +0330

    adding script tags libs styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/bootstrap.css'
+import '../styles/slicker.css'
+import '../styles/animate.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
