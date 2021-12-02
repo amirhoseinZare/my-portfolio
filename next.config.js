ecbc94a2f8cacf3494e47efd0dfd4b4ff66b67b0
amir <zareamirhussein@gmail.com>
2021-12-02 15:10:00 +0330

    adding svg support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
