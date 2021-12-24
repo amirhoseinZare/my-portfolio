f84ffaa3eeee5ec93618500a470771eb17d5fac1
amir <zareamirhussein@gmail.com>
2021-12-24 03:45:57 +0330

    implememt Footer translation

--- pages/comps/Footer.jsx
+++ pages/comps/Footer.jsx
@@ -1,11 +1,14 @@
+import { useTranslation } from "react-i18next"
+
 function Footer(){
+    const { t } = useTranslation()
     return(
         <footer id="footer">
             <div className="footer-items">
-                <div className="footer-item cursor-default has-text-content"><a href="https://www.instagram.com/amir.deve/">Linkedin</a></div>
-                <div className="footer-item cursor-default has-text-content"><a href="https://github.com/amirhussein-zare">GitHub</a></div>
-                <div className="footer-item cursor-default has-text-content"><a href="mailto:zareamirhussein@gmail.com">Email</a></div>
-                <div className="footer-item cursor-default has-text-content"><a href="tel:09035193426">Mobile</a></div>
+                <div className="footer-item cursor-default has-text-content"><a href="https://www.instagram.com/amir.deve/">{t("Footer.Linkedin")}</a></div>
+                <div className="footer-item cursor-default has-text-content"><a href="https://github.com/amirhussein-zare">{t("Footer.GitHub")}</a></div>
+                <div className="footer-item cursor-default has-text-content"><a href="mailto:zareamirhussein@gmail.com">{t("Footer.Email")}</a></div>
+                <div className="footer-item cursor-default has-text-content"><a href="tel:09035193426">{t("Footer.Mobile")}</a></div>
             </div>
         </footer>
     )
