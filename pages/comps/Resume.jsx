11ceb4364ae88a2b591e26da71336a247c425731
amir <zareamirhussein@gmail.com>
2022-01-07 00:44:28 +0330

    some changes

--- pages/comps/Resume.jsx
+++ pages/comps/Resume.jsx
@@ -1,12 +1,49 @@
 import Image from "next/image"
 import { useTranslation } from "react-i18next"
+import { useMemo } from "react"
+import Slider from "react-slick";
 
 function Resume(props){
     const { t } = useTranslation()
+    const settings = useMemo(()=>({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    }), [])
+
     return (
          <section id="resume">
             <h2 className="header-resume change-custom--selection">{t("Resume.Resume")}</h2>
-            <div className="image-slider"  data-slick='{"slidesToShow": 3, "slidesToScroll": 3}' style={{display:"flex", justifyContent:"center"}}>
+            <Slider {...settings} className="image-slider">
                 {props.projects.map(project=>{
                     return (
                         <div key={project.id} className="image-item">
@@ -18,8 +55,7 @@ function Resume(props){
                         </div>
                     )
                 })}
-                
-            </div>
+            </Slider>
         </section>
     )
 }
--- pages/index.page.js
+++ pages/index.page.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Script from 'next/script'
 
+//css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 //libs
-import "../Config/i18n";
+import "../config/i18n";
 
 //comps
 import Hero from './comps/Hero.jsx'
@@ -29,18 +33,18 @@ export default function Home(props) {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" type="image/svg+xml" href="./images/logo.svg" color="#ff0000" />
           <link rel="alternate icon" href="/images/logo.svg" />
-          <link strategy="beforeInteractive" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/bootstrap.css" />
-          <link strategy="beforeInteractive" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/animate.css"/>
-          {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/globals.css" /> */}
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/slicker.css" />
+          {/* <link strategy="beforeInteractive" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/bootstrap.css" /> */}
+          {/* <link strategy="beforeInteractive" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/animate.css"/> */}
+          <link rel="stylesheet" href="https://res.cloudinary.com/ddybs491a/raw/upload/v1641501291/globals_hoajf8.css" />
+          {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/styles/slicker.css" /> */}
           <title>Amirhosein Zare</title>
           <meta name="description" content="Amirhosein Zare Portfolio" />
           <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/jquery.3.5.1.js"/>
-      <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/slick.1.8.1.js"/>
-      <Script strategy="afterInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/slider.js"/>
+      {/* <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/jquery.3.5.1.js"/> */}
+      {/* <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/slick.1.8.1.js"/>
+      <Script strategy="afterInteractive" src="https://cdn.jsdelivr.net/gh/amirhoseinZare/my-portfolio@main/scripts/slider.js"/> */}
       <Script strategy="afterInteractive" src="https://res.cloudinary.com/ddybs491a/raw/upload/v1638459625/sphere_f2haf9.js"/>
       <Script strategy="afterInteractive" src="https://res.cloudinary.com/ddybs491a/raw/upload/v1640300227/document_yxuyxf.js"/>
 
